c7bb3c392c624b8bc08adb4a8cc796071b9d2cab
Josue Hernandez <josuehdz0@gmail.com>
2023-02-28 20:31:35 -0700

    Setting up backend Event Model

---
+++ checkpoint_6_tower/server/models/Event.js
@@ -0,0 +1,20 @@
+import { Schema } from "mongoose";
+import { basicStringType, defaultSchemaOptions } from "../db/Constants.js";
+
+export const EventSchema = new Schema({
+
+  name: basicStringType,
+  description: basicStringType,
+  coverImg: { ...basicStringType, maxLength: 9000, required: true},
+  location: basicStringType,
+  capacity: {type: Number, default: 1},
+  startDate: basicStringType,
+  isCanceled:{ type: Boolean, default: false, required: true},
+  type:{ ...basicStringType, enum: ['concert', 'convention','sport','digital']},
+  
+  // NOTE this is gona be a relationship
+  creatorId:{type: Schema.Types.ObjectId, ref: 'Account', required: true}
+  
+
+
+}, defaultSchemaOptions)
