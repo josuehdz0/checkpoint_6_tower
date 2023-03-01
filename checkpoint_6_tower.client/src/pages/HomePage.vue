1dc70383186c83550e9890318624bdb1f6c65619
Josue Hernandez <josuehdz0@gmail.com>
2023-02-28 17:08:41 -0700

    push

--- checkpoint_6_tower.client/src/pages/HomePage.vue
+++ checkpoint_6_tower.client/src/pages/HomePage.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
     <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
+      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
+        class="rounded-circle">
       <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
         Vue 3 Starter
       </h1>
+      <h1>test for git</h1>
     </div>
   </div>
 </template>
